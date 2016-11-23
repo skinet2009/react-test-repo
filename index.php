bcb05bf45291faba351ca5b8fcb25885ab76a5fc
skinet2009 <skinet788767@gmail.com>
2016-11-23 22:58:07 +0300

    fix minification scripts

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <script type="text/javascript" src="assets/public/js/common.min.js"></script>
-        <script type="text/javascript" src="assets/public/js/main.min.js"></script>
+        <script type="text/javascript" src="assets/public/js/common.js"></script>
+        <script type="text/javascript" src="assets/public/js/main.js"></script>
         <link href="assets/public/css/main.css" rel="stylesheet">
         <meta charset="utf-8">
         <title></title>
